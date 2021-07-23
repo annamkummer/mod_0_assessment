103d980ecebeedd5c07c80ec8c755b7995351aef
Anna Kummer <annamkummer@gmail.com>
2021-07-23 08:48:43 -0600

    Add airport object

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var airportInfo = {
+  name: "Hector International",
+  city: "Fargo, ND",
+  numOfGates: 5,
+  numOfCoffeeShops: 1,
+  isInternational: true,
+  hasGiftShop: true,
+  airlines: ["United", "Frontier", "Spirit"],
+  destinations: ["Minneapolis", "Denver", "Chicago", "Toronto"]
+};
