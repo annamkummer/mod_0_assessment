4c99baf97a1a26dba6f65d988f8b0a9e47a53bf2
Anna Kummer <annamkummer@gmail.com>
2021-07-23 08:41:51 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,14 @@
+var famousPerson = "Kriten Wiig";
+var isActive = true;
+
+famousPerson = "Maya Rudolph";
+
+var num3 = 2;
+var num9 = 6;
+var sum = num3 + num9;
+
+var animals = ["bat", "cat", "dog"];
+
+animals.shift();
+
+console.log(famousPerson.length);
