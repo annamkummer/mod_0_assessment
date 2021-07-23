23151ec1ee439025bbaef1b02d310e7a18411255
Anna Kummer <annamkummer@gmail.com>
2021-07-23 08:55:10 -0600

    Add vehicle object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var vehicleSpecs = {
+  make: "Toyota",
+  model: "Rav4",
+  mpg: 37,
+  year: 2019,
+  isHybrid: true,
+  hasTrailorHitch: false,
+  techFeatures: ["EV Mode", "Backup Camera", "Lane Assist", "Adaptive Cruise"],
+  engineFeatures: ["4 Cylinder", "Hybrid"]
+};
